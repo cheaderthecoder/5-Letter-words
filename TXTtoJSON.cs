98b0682b5f63987e1da5d710114b213f621219ad
cheaderthecoder <143862092+cheaderthecoder@users.noreply.github.com>
2024-08-24 09:04:48 +0300

    add c# script

---
+++ TXTtoJSON.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace Test_c_
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            var words = await LoadWords();
+
+            var json = JsonConvert.SerializeObject(words);
+
+            Console.WriteLine(json);
+            Console.ReadKey();
+
+            File.WriteAllText("words.json", json);
+        }
+
+        public static async Task<List<string>> LoadWords()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                Console.WriteLine("Connecting");
+                string content = await client.GetStringAsync("https://cheaderthecoder.github.io/5-Letter-words/words.txt");
+                string[] lines = content.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                return lines.ToList();
+            }
+        }
+    }
+}
